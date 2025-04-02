0498c3a7030ec2b9dc0efd7f103a981c71bb94fa
Dani <daniel.meridacordero@kyndryl.com>
2025-04-02 11:01:00 +0200

    nuevas class Data

--- Data/AniListAuth.py
+++ Data/AniListAuth.py
@@ -1,10 +1,11 @@
 import requests
+import webbrowser
 from urllib.parse import urlencode
 
 class AniListAuth:
-    auth_url = "https://anilist.co/api/v2/oauth/authorize"
-    token_tmp = "https://anilist.co/api/v2/oauth/token"
-    api_url = "https://graphql.anilist.co"
+    AUTH_URL = "https://anilist.co/api/v2/oauth/authorize"
+    TOKEN_URL = "https://anilist.co/api/v2/oauth/token"
+    API_URL = "https://graphql.anilist.co"
 
     def __init__(self, client_id, client_secret, redirect_uri):
         self.client_id = client_id
@@ -18,7 +19,7 @@ class AniListAuth:
             "redirect_uri": self.redirect_uri,
             "response_type": "code",
         }
-        return f"{self.auth_url}?{urlencode(params)}"
+        return f"{self.AUTH_URL}?{urlencode(params)}"
 
     def get_access_token(self, auth_code):
         data = {
@@ -28,22 +29,7 @@ class AniListAuth:
             "redirect_uri": self.redirect_uri,
             "code": auth_code,
         }
-        response = requests.post(self.token_tmp, data=data)
+        response = requests.post(self.TOKEN_URL, data=data)
         token_data = response.json()
         self.access_token = token_data.get("access_token")
         return self.access_token
-
-    def get_user_data(self):
-        if not self.access_token:
-            raise ValueError("Access token is required.")
-        query = """
-        query {
-            Viewer {
-                id
-                name
-            }
-        }
-        """
-        headers = {"Authorization": f"Bearer {self.access_token}"}
-        response = requests.post(self.api_url, json={"query": query}, headers=headers)
-        return response.json()
--- Data/NASA_API.py
+++ Data/NASA_API.py
@@ -1,26 +1,29 @@
 import requests
-import webbrowser
 
-from urllib.parse import urlencode
 class NASA_API:
     BASE_URL = "https://api.nasa.gov"
     API_KEY = "13kRF4pk6oMPewjkKx4R7i4h9el95QMzAMGGHyww"
 
+    # Metodo para obtener la 'Astronomy Picture of the Day' (APOD) e información de la NASA .
     def get_apod(self):
         url = f"{self.BASE_URL}/planetary/apod?api_key={self.API_KEY}"
         response = requests.get(url)
         return response.json()
 
+    # Metodo para obtener un feed diario de asteroides cercanos a la Tierra
     def get_asteroid_info(self):
         url = f"{self.BASE_URL}/neo/rest/v1/feed?api_key={self.API_KEY}"
         response = requests.get(url)
         return response.json()
 
+    # Metodo para obtener una lista de patentes relacionadas con la transferencia de tecnología de la NASA.
     def get_exploration_reports(self):
-        url = f"{self.BASE_URL}/techport/api/projects?api_key={self.API_KEY}"
+        # Nuevo endpoint para patentes de transferencia de tecnología
+        url = f"{self.BASE_URL}/techtransfer/patent/?engine&api_key={self.API_KEY}"
         response = requests.get(url)
         return response.json()
 
+    # Método para obtener una imagen satelital de la Tierra en coordenadas específicas.
     def get_earth_image(self, lat, lon, dim=0.1):
         url = f"{self.BASE_URL}/planetary/earth/imagery?lon={lon}&lat={lat}&dim={dim}&api_key={self.API_KEY}"
         response = requests.get(url)
